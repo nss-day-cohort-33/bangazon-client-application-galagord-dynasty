7a66beb023040ea6e3ea1bb272f707ee722b6f1e
Shane Miller <shanemiller89@gmail.com>
2019-10-06 21:27:41 -0500

    intial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import BangazonApp from './components/BangazonApp';
+
+ReactDOM.render(<BangazonApp />, document.getElementById('root'));
