699e8c80c484a645d27797214dfc8d2e71069a55
krystal gates <krystal_sabrina@yahoo.com>
2019-10-08 14:48:57 -0500

    work on add product to order

--- src/components/product/ProductDetail.js
+++ src/components/product/ProductDetail.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react"
+import useSimpleAuth from "../../hooks/ui/useSimpleAuth"
 
 // Author: Krystal Gates
 // Purpose: Show product detail
 
 const ProductDetail = props => {
     const [product, setProducts] = useState([])
+    const [order, setOrder] = useState([])
+    const { isAuthenticated } = useSimpleAuth()
 
 
 
@@ -22,12 +25,61 @@ const ProductDetail = props => {
     }
 
     const getCurrentOrder = (props) => {
-        fetch(`http://localhost:8000/orders/1`,{
+        console.log("current order")
+        fetch(`http://localhost:8000/orders?customer=${localStorage.getItem("customer_id")}&payment_id=null`,{
             "method": "GET",
             "headers": {
                 "Accept": "application/json",
+                 "Authorization": `Token ${localStorage.getItem("bangazon_token")}`
             }
         })
+            .then(response => response.json())
+            .then((order) => {
+                setOrder(order)
+            })
+    }
+
+    const makeNewOrder = (props) => {
+        console.log("make new order")
+        return fetch("http://localhost:8000/orders", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": `Token ${localStorage.getItem("bangazon_token")}`
+              },
+              body: JSON.stringify({"customer_id": `${localStorage.getItem(`customer_id`)}`})
+            }).then(res => res.json());
+    }
+
+    const addOrderProduct = (props) => {
+        return fetch("http://localhost:8000/orderproducts", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": `Token ${localStorage.getItem("bangazon_token")}`
+              },
+              body: JSON.stringify({"customer_id": `${localStorage.getItem(`customer_id`)}`})
+            }).then(res => res.json());
+    }
+
+    const addProductToOrder = (props) => {
+        console.log("hello")
+        if (isAuthenticated()) {
+            if (getCurrentOrder()){
+                console.log("order is open")
+                // .then(addOrderProduct())
+            }
+            else{
+                console.log("no order")
+                makeNewOrder()
+                // addOrderProduct()
+            }
+        }
+        else{
+            alert("You are not logged in!")
+        }
     }
 
     useEffect(() => {getProduct(props.productDetailId)}, [])
@@ -39,7 +91,7 @@ const ProductDetail = props => {
                 <h3>Price: {product.price}</h3>
                 <h4>Quantity Available: {product.quantity}</h4>
                 <p>Description: {product.description}</p>
-                <button>Add To Order</button>
+                <button onClick={addProductToOrder}>Add To Order</button>
             </main>
         </>
     )
