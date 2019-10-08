506d1acbd9d6d749ff632177608ebae1e6c6297e
krystal gates <krystal_sabrina@yahoo.com>
2019-10-08 13:17:47 -0500

    add home in nav bar

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -11,6 +11,9 @@ const NavBar = props => {
             <div style={{fontSize: "2em", marginLeft: "1em"}}><strong>Welcome To Bangazon</strong></div>
             <ul className="nav nav-pills nav-fill">
                 <li className="nav-item">
+                    <Link className="nav-link" to="/">Home</Link>
+                </li>
+                <li className="nav-item">
                     <Link className="nav-link" to="/categorylist">Category List</Link>
                 </li>
                 <li>
