077a8a86c7889c75d19dbdf2cfd3ff9843079234
Misty DeRamus <mistyderamus@gmail.com>
2019-10-08 09:53:58 -0500

    updated navbar and home page

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -11,10 +11,13 @@ const NavBar = props => {
             <div style={{fontSize: "2em", marginLeft: "1em"}}><strong>Welcome To Bangazon</strong></div>
             <ul className="nav nav-pills nav-fill">
                 <li className="nav-item">
-                    <Link className="nav-link" to="/">INSERT LINK HERE</Link>
+                    <Link className="nav-link" to="/">Categories</Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="/">INSERT LINK HERE</Link>
+                    <Link className="nav-link" to="/">Sell</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/">Profile</Link>
                 </li>
                 {
                     isAuthenticated() ?
