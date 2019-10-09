cebb7c1fc97ea3df0e635cde9776829107de0723
Misty DeRamus <mistyderamus@gmail.com>
2019-10-09 08:29:12 -0500

    add payment link

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -8,6 +8,7 @@ import ProductForm from "./product/ProductForm"
 import CategoryList from "./category/CategoryList"
 import ProductList from "./home/ProductList"
 import CustomerProfile from "./payment/CustomerProfile"
+import PaymentForm from "./payment/PaymentForm"
 
 const ApplicationViews = () => {
     return (
@@ -68,6 +69,16 @@ const ApplicationViews = () => {
                         <>
                             <h3>Customer Profile</h3>
                             <CustomerProfile {...props}/>
+                        </>
+                    )
+                }}
+            /> }
+            {<Route
+                path="/PaymentForm" render={props => {
+                    return (
+                        <>
+                            <h3>Payment Form</h3>
+                            <PaymentForm {...props}/>
                         </>
                     )
                 }}
