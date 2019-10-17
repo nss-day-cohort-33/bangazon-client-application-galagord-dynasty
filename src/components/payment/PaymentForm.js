d2d3f45a2e15fc6c353903ff7f31a63eb7467207
Karla Gallegos <kgalleg618@gmail.com>
2019-10-17 10:35:43 -0500

    user gets an alert if they enter an expiration date that is in the past

--- src/components/payment/PaymentForm.js
+++ src/components/payment/PaymentForm.js
@@ -1,9 +1,10 @@
+//Author: Karla Gallegos
+//Purpose: Allows user to post payment types
+//Methods: POST
 
-
-import React, { useState, useEffect, useRef } from "react"
-import { Link } from "react-router-dom"
-import useSimpleAuth from "../../hooks/ui/useSimpleAuth"
-
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import useSimpleAuth from "../../hooks/ui/useSimpleAuth";
 
 const PaymentForm = props => {
   const merchant_name = useRef();
@@ -13,65 +14,79 @@ const PaymentForm = props => {
   const { isAuthenticated } = useSimpleAuth();
 
   const createPayment = e => {
-      e.preventDefault();
-      const created = `${expiration_date.current.value}-01`
-      if (isAuthenticated()) {
-          fetch(`http://localhost:8000/payments`, {
-              "method": "POST",
-              "headers": {
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-                "Authorization": `Token ${localStorage.getItem("bangazon_token")}`
-              },
-              "body": JSON.stringify({
-              "merchant_name": merchant_name.current.value,
-              "account_number": account_number.current.value,
-              "expiration_date": expiration_date.current.value,
-              "created_at": created
+    e.preventDefault();
+    const created = `${expiration_date.current.value}-01`;
+    if (isAuthenticated()) {
+      fetch(`http://localhost:8000/payments`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Token ${localStorage.getItem("bangazon_token")}`
+        },
+        body: JSON.stringify({
+          merchant_name: merchant_name.current.value,
+          account_number: account_number.current.value,
+          expiration_date: expiration_date.current.value,
+          created_at: created
+        })
+      })
+        .then(response => response.json())
+        .then(response => {
+          if ("error" in response == true) {
+            alert("Your card is expired");
+          } else {
+            props.history.push("/PaymentList");
+          }
+        });
+    }
+  };
 
-          })
-          })
-              .then(response => response.json())
-              .then((response) => {
-                // console.log("error" in response)
-                if("error" in response === true){
-                  alert("The expiration date is in the past")
-                } else{
-                  props.history.push("/")
-                }
-              })
-
-      }
-  }
-
-
-return (
-  <>
-    <h1>Create a Payment Option</h1>
-    <form className="categoryList" onSubmit={
-      createPayment
-    }>
-    <fieldset>
+  return (
+    <>
+      <h1>Create a Payment Option</h1>
+      <form className="categoryList" onSubmit={createPayment}>
+        <fieldset>
           <label htmlFor="merchant_name">Merchant Name:</label>
-          <input type="text" ref={merchant_name} name="merchant_name" required></input>
+          <input
+            type="text"
+            ref={merchant_name}
+            name="merchant_name"
+            required
+          ></input>
         </fieldset>
 
         <fieldset>
           <label htmlFor="account_number">Account Number:</label>
-          <input type="text" ref={account_number} name="account_number" required></input>
+          <input
+            type="text"
+            ref={account_number}
+            name="account_number"
+            required
+          ></input>
         </fieldset>
-
 
         <fieldset>
-          <label htmlFor="exp_date">Expiration Date:</label>
-          <input type="date" ref={expiration_date} name="expiration_date" min={new Date()} required></input>
+          <label htmlFor="expiration_date">Expiration Date:</label>
+          <input
+            type="date"
+            ref={expiration_date}
+            name="expiration_date"
+            min={new Date()}
+            required
+          ></input>
         </fieldset>
-        <input type="date" ref={created_at} name="expiration_date" defaultValue={new Date()} hidden></input>
+        <input
+          type="date"
+          ref={created_at}
+          name="expiration_date"
+          defaultValue={new Date()}
+          hidden
+        ></input>
         <button type="submit">Add Payment</button>
       </form>
-  </>
+    </>
+  );
+};
 
-)
-}
-
-export default PaymentForm
+export default PaymentForm;
--- src/components/payment/PaymentOptions.js
+++ src/components/payment/PaymentOptions.js
@@ -1,7 +1,9 @@
+//Author: Karla Gallegos
+//Purpose: Allows get and view all payment options posted
+//Methods: GET PAYMENT OPTIONS
 
 import React, { useEffect, useState } from "react";
-import DeletePayment from "./DeletePayment"
-
+import DeletePayment from "./DeletePayment";
 
 const PaymentOptions = props => {
   const [paymenttypes, setPaymenttypes] = useState([]);
@@ -10,32 +12,26 @@ const PaymentOptions = props => {
     fetch("http://localhost:8000/payments", {
       method: "GET",
       headers: {
-        "Accept": "application/json",
+        Accept: "application/json",
         "Content-Type": "application/json",
-        "Authorization": `Token ${localStorage.getItem("bangazon_token")}`
-
+        Authorization: `Token ${localStorage.getItem("bangazon_token")}`
       }
     })
       .then(res => res.json())
       .then(setPaymenttypes);
   }, []);
 
-
   return (
     <>
       <main className="explorer">
-
-      <section className="product">
-                {
-                    paymenttypes.map(payment =>
-                        <DeletePayment key={payment.id}
-                            payment={payment} {...props} />)
-                }
-      </section>
-
+        <section className="product">
+          {paymenttypes.map(payment => (
+            <DeletePayment key={payment.id} payment={payment} {...props} />
+          ))}
+        </section>
       </main>
     </>
   );
 };
 
-export default PaymentOptions
+export default PaymentOptions;
